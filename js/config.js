b04096649057d340e48bbafc45e8c368f45e9b0c
Tong Xiang <tong.xiang2@gmail.com>
2015-02-26 14:51:10 -0500

    :wadd preloaded science sleuth game

--- js/config.js
+++ js/config.js
@@ -1,4 +1,7 @@
 // Contains "global" variables. 
 
 var START_LEVEL_KEY = "L10"
-  , TOTAL_NUMBER_OF_LEVELS = 6;
+  , PRELOADED_STORY_NAME = "Science_Sleuth_2014"
+  , PRELOADED_STORY_FILENAME = "sciencesleuth2014.html"
+  , TOTAL_NUMBER_OF_LEVELS = 6
+  ;
