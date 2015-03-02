34c4ecb8f39184c2d9cfa58971c4b2c4d12ca189
Tong Xiang <tong.xiang2@gmail.com>
2015-03-02 17:37:16 -0500

    add blank template game to preloader

--- js/config.js
+++ js/config.js
@@ -1,7 +1,9 @@
 // Contains "global" variables. 
 
 var START_LEVEL_KEY = "L10"
-  , PRELOADED_STORY_NAME = "Science_Sleuth_2014"
-  , PRELOADED_STORY_FILENAME = "sciencesleuth2014.html"
   , TOTAL_NUMBER_OF_LEVELS = 6
+  , PRELOADS = [
+    {"name": "Science_Sleuth_2014", "filename": "sciencesleuth2014.html"},
+    {"name": "blank_game_template (sci sleu format)", "filename": "blankgametemplate.html"}
+  ]
   ;
--- preload/preload.js
+++ preload/preload.js
@@ -1,16 +1,22 @@
-var autoImportFile = function(){
-  $.get(window.location.pathname + 'preload/' + PRELOADED_STORY_FILENAME, function(response) {
-    var preloadHasLoaded = false;
-    StoryCollection.all().each(function(story){
-      if (story.attributes.name == PRELOADED_STORY_NAME) {
-        preloadHasLoaded = true;
-      }
-    })
+var autoImportFiles = function(){
+  var i,
+      file,
+      preloadHasLoaded;
+  for (i = 0; i < PRELOADS.length; i++){
+    file = PRELOADS[i];
+    preloadHasLoaded = false;
+    $.get(window.location.pathname + 'preload/' + file.filename, function(response) {
+      StoryCollection.all().each(function(story){
+        if (story.attributes.name == file.name) {
+          preloadHasLoaded = true;
+        }
+      })
 
-    if (!preloadHasLoaded){
-      window.app.importFile(response);
-    }
-  })
+      if (!preloadHasLoaded){
+        window.app.importFile(response);
+      }
+    }) 
+  }
 }
 
-$.ready(autoImportFile());
+$.ready(autoImportFiles());
