7f9e61e38faecb2de9cb1e883c111572ebcfd364
Tong Xiang <tong.xiang2@gmail.com>
2015-09-21 10:48:02 -0400

    science sleuth 2015

--- js/config.js
+++ js/config.js
@@ -6,6 +6,7 @@ var START_LEVEL_KEY = "L10"
     {"name": "Science Sleuth 2014", "filename": "sciencesleuth2014.html"},
     {"name": "Blank Game Template (sci sleu format)", "filename": "blankgametemplate.html"},
     {"name": "The Bully Text: Talent Show-Off", "filename": "bullytexttalentshowoff2015.html"},
-    {"name": "WWE Bully Text 2015", "filename": "WWEBullyText2015.html"}
+    {"name": "WWE Bully Text 2015", "filename": "WWEBullyText2015.html"},
+    {"name": "Science Sleuth 2015", "filename": "sciencesleuth2015.html"}
   ]
   ;
